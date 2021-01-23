42d2bc2b9b862d80edd6af6154daa67641e737d8
Alex <asobiloff@gmail.com>
2021-01-23 12:53:45 -0800

    added swatch panel, bottom circle and top bar

---
+++ components/SwatchPanel.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import Swatch from './Swatch';
+
+type Props = {
+  activeColor: string;
+}
+
+const SwatchPanel = ({
+  activeColor,
+}: Props) => {
+  const initialColorHistory = [
+    activeColor,
+    activeColor,
+    activeColor,
+    activeColor,
+    activeColor,
+  ];
+  const [colorHistory, setColorHistory] = useState(initialColorHistory);
+  const onChangeComplete = () => {
+    const newHistory = [activeColor, ...colorHistory].slice(0,5);
+
+    setColorHistory(newHistory);
+  }
+
+  useEffect(() => { onChangeComplete(); }, [activeColor]);
+
+  return (
+    <View style={styles.Container}>
+    {
+      colorHistory.map((color, idx) => (
+        <Swatch
+          key={`Swatch_${color}_${idx}`}
+          color={color}
+        />
+      ))
+    }
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  Container: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    width: '100%',
+  },
+})
+
+export default SwatchPanel;
